4fc97705cb1a6d21a5349be4431fa5060f23aa1b
Edi Aquino de Souza <edsouzza@hotmail.com>
2023-05-22 10:44:47 -0300

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wdev/vagas',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '31050431fc9f7c9bd903254c467c2ba8d3838814',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wdev/vagas' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '31050431fc9f7c9bd903254c467c2ba8d3838814',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
